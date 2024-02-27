98d84a710de9267d857b95dc2815e4a5b42ca470
Kojer <134270504+QuocKo@users.noreply.github.com>
2024-02-28 00:15:57 +0700

    GetEquipmentAll
    Get Euipment information

---
+++ ManageCourseCenter_BE/MCC.API/Controllers/EquipmentController.cs
@@ -0,0 +1,23 @@
+﻿using MCC.DAL.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCC.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EquipmentController : ControllerBase
+{
+    private IEquipmentService _equipService;
+
+    public EquipmentController(IEquipmentService equipService)
+    {
+        _equipService = equipService;
+    }
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAllEquipmentAsync()
+    {
+        var result = await _equipService.GetAllEquipmentAsync();
+        return Ok(result);
+    }
+}
---
+++ ManageCourseCenter_BE/MCC.DAL/Service/Implements/EquipmentService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MCC.DAL.Common;
+using MCC.DAL.Repository.Interface;
+using MCC.DAL.Service.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCC.DAL.Service.Implements
+{
+    public class EquipmentService : IEquipmentService
+    {
+        private IEquipmentRepository _equipRepo;
+        private IMapper _mapper;
+
+        public EquipmentService(IEquipmentRepository equipRepo, IMapper mapper)
+        {
+            _equipRepo = equipRepo;
+            _mapper = mapper;
+        }
+
+        public async Task<AppActionResult> GetAllEquipmentAsync()
+        {
+            var actionResult = new AppActionResult();
+            var data = await _equipRepo.GetAllAsync();
+            return actionResult.BuildResult(data);
+        }
+    }
+}

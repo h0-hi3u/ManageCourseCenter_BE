e4dd6a8b28cdf36707c610a465ca2d49441c2746
h0-hi3u <hieuenglishabc@gmail.com>
2024-02-28 20:30:50 +0700

    API get equimentActivity
    get by equipId, managerId, roomNo, roomId, timeRange

--- ManageCourseCenter_BE/MCC.DAL/Repository/Implements/EquipmentActivityRepository.cs
+++ ManageCourseCenter_BE/MCC.DAL/Repository/Implements/EquipmentActivityRepository.cs
@@ -1,6 +1,7 @@
 ﻿using MCC.DAL.DB.Context;
 using MCC.DAL.DB.Models;
 using MCC.DAL.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace MCC.DAL.Repository.Implements;
 
@@ -8,5 +9,30 @@ public class EquipmentActivityRepository : RepositoryGeneric<EquipmenntActivity>
 {
     public EquipmentActivityRepository(ManageCourseCenterContext context) : base(context)
     {
+    }
+
+    public async Task<IEnumerable<EquipmenntActivity>> GetActivitiesByEquipmentIdAsync(int equipId)
+    {
+        return await _dbSet.Where(ea => ea.EquipmentId == equipId).ToListAsync();
+    }
+
+    public async Task<IEnumerable<EquipmenntActivity>> GetActivitiesByManagerIdAsync(int managerId)
+    {
+        return await _dbSet.Where(ea => ea.ManagerId == managerId).ToListAsync();
+    }
+
+    public async Task<IEnumerable<EquipmenntActivity>> GetActivitiesByRoomIdAsync(int roomId)
+    {
+        return await _dbSet.Where(ea => ea.RoomId == roomId).ToListAsync();
+    }
+
+    public async Task<IEnumerable<EquipmenntActivity>> GetActivitiesByRoomNoAsync(int roomNo)
+    {
+        return await _dbSet.Where(ea => ea.Room.RoomNo == roomNo).ToListAsync();
+    }
+
+    public async Task<IEnumerable<EquipmenntActivity>> GetActivitiesByTimeRangeAsync(DateTime from, DateTime to)
+    {
+        return await _dbSet.Where(ea => ea.OperateTime >= from && ea.OperateTime <= to).ToListAsync();
     }
 }

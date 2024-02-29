ec680a9dcf292f7782cda89b7aed03755fa3ecea
Kojer <134270504+QuocKo@users.noreply.github.com>
2024-02-29 14:41:58 +0700

    getReportByID getReportByEuipmentID getReportByEuipmentName getReportByRoomNo getReportByRoomID
    getReportByID
    getReportByEuipmentID
    getReportByEuipmentName
    getReportByRoomNo
    getReportByRoomID

--- ManageCourseCenter_BE/MCC.DAL/Service/Interface/IEquipmentReportService.cs
+++ ManageCourseCenter_BE/MCC.DAL/Service/Interface/IEquipmentReportService.cs
@@ -10,5 +10,10 @@ namespace MCC.DAL.Service.Interface
     public interface IEquipmentReportService
     {
         Task<AppActionResult> GetAllEquipmentReportAsync();
+        Task<AppActionResult> GetEquipmentReportByIdAsync(int id);
+        Task<AppActionResult> GetEquipmentReportByEquipmentIdAsync(int equipmentid);
+        Task<AppActionResult> GetEquipmentReportByRoomIdAsync(int roomid);
+        Task<AppActionResult> GetEquipmentReportByRoomNoAsync(int roomno);
+        Task<AppActionResult> GetEquipmentReportByEquipmentNameAsync(string equipmentname);
     }
 }

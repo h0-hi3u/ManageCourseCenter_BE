cdac0957335d49f43a00ee26c1fce9f260bc635b
Leo <nghia2852002@gmail.com>
2024-03-05 21:03:34 +0700

    updateCourseInformation Api

---
+++ ManageCourseCenter_BE/MCC.DAL/Dto/CourseDto/CourseUpdateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCC.DAL.Dto.CourceDto;
+
+public class CourseUpdateDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public DateTime OpenFormTime { get; set; }
+    public DateTime CloseFormTime { get; set; }
+    public double? Price { get; set; }
+    public int Level { get; set; }
+    public int TotalSlot { get; set; }
+    public int Status { get; set; }
+}

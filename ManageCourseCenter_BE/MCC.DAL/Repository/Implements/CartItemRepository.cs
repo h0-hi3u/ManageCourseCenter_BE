56e34cf9449fa84519d8c2b835f6146604ac491a
Leo <nghia2852002@gmail.com>
2024-03-11 11:21:03 +0700

    deleteCartItem

--- ManageCourseCenter_BE/MCC.DAL/Repository/Implements/CartItemRepository.cs
+++ ManageCourseCenter_BE/MCC.DAL/Repository/Implements/CartItemRepository.cs
@@ -11,6 +11,19 @@ public class CartItemRepository : RepositoryGeneric<CartItem>, ICartItemReposito
     {
     }
 
+    public async Task<bool> DeleteCartItemAsync(int cartItemId)
+    {
+        var cartItem = await _context.CartItems.FindAsync(cartItemId);
+        if (cartItem == null)
+        {
+            return false;
+        }
+
+        _context.CartItems.Remove(cartItem);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
     public async Task<IEnumerable<CartItem>> getCartItemByParentIDAsync(int parentId)
     {
         var cartItems = await _dbSet
